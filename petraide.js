e94458623ba5bf345d3c18f18262edbfdc01920e
petrasostaric12-ai <petrasostaric12@gmail.com>
2026-09-10 16:32:10 +0200

    Add files via upload

---
+++ petraide.js
@@ -0,0 +1,38 @@
+//prvi zadatak
+console.log("hello world");
+
+
+//drugi zadatak
+let br1=5;
+let br2=7;
+let zbroj=br1+br2;
+
+console.log("zbroj je"+zbroj);
+
+
+//deveti zadatak
+
+let ime=petra;
+let prezime=sostaric;
+console.log("bok,"+ime+prezime);
+
+//12 zadatak
+
+for (let i=1;i>=10;i--){
+    console.log(i);
+}
+
+    //4. zadatak
+
+
+    for (let i=1;i<=10;i++){
+    console.log(i);
+}
+
+//7. zadatak
+let a=5;
+let b=6;
+let c=1;
+let rezultat=Math.max(a,b,c);
+console.log("najveci broj je "+ rezultat);
+
